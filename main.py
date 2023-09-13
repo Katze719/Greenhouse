b7031ceef17f355f944fd7744c95c824d4da3d78
Paul Dorn <pauldorn1234@gmail.com>
2023-09-13 14:07:57 +0200

    test

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ while True:
 
     segment[0] = str(result.temperature)[0]
     segment[1] = str(result.temperature)[1]
-    segment[2] = str(result.temperature)[3]
+    segment[2] = str(result.temperature)[3] & 0b10000000
     segment[3] = 'C'
 
     segment.colon = False
