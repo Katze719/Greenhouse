f51d216ccd548a3f6b7dccdc38852818afc67508
Paul Dorn <pauldorn1234@gmail.com>
2023-09-13 13:44:12 +0200

    addded loop

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ this is the main python file
 
 import RPi.GPIO as GPIO
 import dht11
+import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 instance = dht11.DHT11(pin = 4)
-result = instance.read()
 
-while not result.is_valid():
+while True:
     result = instance.read()
 
-print("Temperatur: %-3.1f C" % result.temperature)
+    while not result.is_valid():
+        result = instance.read()
+
+    print("Temperatur: %-3.1f C" % result.temperature)
+    print("Feuchtigkeit: %-3.1f %%" % result.humidity)
+    
+    time.sleep(0.5)
 
 
 print("hello world")
