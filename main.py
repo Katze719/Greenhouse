9ce2930943442a296e175616be35e7fd6d709726
Paul Dorn <pauldorn1234@gmail.com>
2023-09-13 14:01:31 +0200

    test segment 7

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ this is the main python file
 import RPi.GPIO as GPIO
 import dht11
 import time
+import board
+from adafruit_ht16k33.segments import Seg7x4
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 instance = dht11.DHT11(pin = 4)
+
+i2c = board.I2C()
+segment = Seg7x4(i2c, address=0x70)
+segment.fill(0)
+
 
 while True:
     time.sleep(1)
@@ -25,4 +32,8 @@ while True:
 
     print("Temperatur: %-3.1f C" % result.temperature)
     print("Feuchtigkeit: %-3.1f %%" % result.humidity)
+
+    segment[0] = str(result.temperature)[0]
+
+    segment.show()
     
