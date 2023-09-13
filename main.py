582afba06e98f459bea733e52d087d01d3d15e47
Paul Dorn <pauldorn1234@gmail.com>
2023-09-13 13:29:29 +0200

    add temp test

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ main.py
 this is the main python file
 """
 
-import modules.test as t
+import RPI.GPIO as GPIO
+import dht11
 
-t.test()
+GPIO.setwarning(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+instance = dht11.DH11(pin = 4)
+result = instance.read()
+
+while not result.is_valid():
+    result = instance.read()
+
+print("Temperatur: %-3.1f C" % result.temperature)
+
 
 print("hello world")
