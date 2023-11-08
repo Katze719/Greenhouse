484b338d2a79bcb8b84c7839096e4129b2cb34eb
Katze719 <pauldorn1234@gmail.com>
2023-11-08 14:35:46 +0100

    add

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main(stdscr):
             segment[1] = str(result.humidity)[1]
             segment[1] = str(result.humidity)[2]
             segment[2] = str(result.humidity)[3]
-            segment.set_digit_raw(3, 0b11111111)
+            segment.set_digit_raw(3, 0b11110011)
 
 
         # Daten auf der 7 segment anzeige aktuallisieren (anzeigen)
