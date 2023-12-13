91ee2af50b5d1c80dabe2174987bc0a4ef318145
Katze719 <pauldorn1234@gmail.com>
2023-12-13 17:39:24 +0100

    add version vor multy processing

--- multi.py
+++ multi.py
@@ -5,6 +5,7 @@
 import RPi.GPIO as GPIO
 import dht11
 import time
+import multiprocessing 
 import curses
 import board
 import busio
@@ -47,6 +48,11 @@ if(GPIO.RPI_REVISION == 1):
     bus = smbus.SMBus(0)
 else:
     bus = smbus.SMBus(1)
+
+thread_light_sensor: multiprocessing.Process = None
+thread_temp_humid_sensor: multiprocessing.Process = None
+
+programm_run = True
 
 class LightSensor():
 
@@ -150,8 +156,6 @@ def main(stdscr):
         stdscr.addstr(line_number, 12, "->")
         stdscr.addstr(line_number, 15, description)
 
-    # Initialisiert die Anzahl der Messungen.
-    measurements = 0
 
     # Blendet den Cursor in der Terminalausgabe aus.
     curses.curs_set(0)
@@ -161,71 +165,85 @@ def main(stdscr):
     addKeyDescriptionToTerminal(5, "Linksklick", "Konsole Pausieren (Programm läuft im Hintergrund weiter)")
     addKeyDescriptionToTerminal(6, "Rechtsklick", "Konsole Weiter")
 
-    # Eine Variable, um zwischen Temperatur und Feuchtigkeit auf dem 7-Segment-Display zu wechseln.
-    change = 0
+    def startLightSensor():
+        # Initialisiert die Anzahl der Messungen.
+        measurements = 0
 
-    # Speichert die Zeit der letzten Anzeigeaktualisierung.
-    last_display_time = time.time()
+        matrix_field = Matrix(cascaded=1, block_orientation=90, rotate=0)
+        matrix_field.showPattern("smiley")
 
-    light_sensor = LightSensor()
+        light_sensor = LightSensor()
 
-    matrix_field = Matrix(cascaded=1, block_orientation=90, rotate=0)
+        while programm_run:
+            addDataLineToTerminal(4, "Heligkeit:", f"{light_sensor.readLight()} lx")
+            addDataLineToTerminal(5, "Messung light:", f"{measurements}")
 
-    matrix_field.showPattern("smiley")
+    def startTempAndHumidSensor():
+        # Initialisiert die Anzahl der Messungen.
+        measurements = 0
 
-    while True:
-        # Verzögert die Schleife um 1 Sekunde.
-        time.sleep(1)
+        # Eine Variable, um zwischen Temperatur und Feuchtigkeit auf dem 7-Segment-Display zu wechseln.
+        change = 0
 
-        # Speichert die aktuelle Zeit.
-        current_time = time.time()
+        # Speichert die Zeit der letzten Anzeigeaktualisierung.
+        last_display_time = time.time()
 
-        # Wechselt alle 5 Sekunden zwischen Temperatur und Feuchtigkeit.
-        if current_time - last_display_time >= 5:
-            change = (change + 1) % 2
-            last_display_time = current_time
+        while programm_run:
+            # Speichert die aktuelle Zeit.
+            current_time = time.time()
 
-        # Inkrementiert die Messanzahl.
-        measurements += 1
+            # Wechselt alle 5 Sekunden zwischen Temperatur und Feuchtigkeit.
+            if current_time - last_display_time >= 5:
+                change = (change + 1) % 2
+                last_display_time = current_time
 
-        # Liest die Daten vom DHT11-Sensor.
-        result = instance.read()
+            # Inkrementiert die Messanzahl.
+            measurements += 1
 
-        # Wiederholt das Lesen, bis gültige Daten vorliegen.
-        while not result.is_valid():
+            # Liest die Daten vom DHT11-Sensor.
             result = instance.read()
 
-        # Zeigt die gemessenen Werte im Terminal an.
-        addDataLineToTerminal(0, "Temperatur:", f"{result.temperature} C")
-        addDataLineToTerminal(1, "Feuchtigkeit:", f"{result.humidity} %")
-        addDataLineToTerminal(2, "Heligkeit:", 30 * ' ')
-        addDataLineToTerminal(2, "Heligkeit:", f"{light_sensor.readLight()} lx")
-        addDataLineToTerminal(3, "Messung:", f"{measurements}")
-        stdscr.refresh()
+            # Wiederholt das Lesen, bis gültige Daten vorliegen.
+            while not result.is_valid():
+                result = instance.read()
 
-        # Aktualisiert das 7-Segment-Display mit den Sensorwerten.
-        if change == 0:
-            segment[0] = str(result.temperature)[0]
-            segment[1] = str(result.temperature)[1]
-            segment[1] = str(result.temperature)[2]
-            segment[2] = str(result.temperature)[3]
-            segment[3] = 'C'
-        else:
-            segment[0] = str(result.humidity)[0]
-            segment[1] = str(result.humidity)[1]
-            segment[1] = str(result.humidity)[2]
-            segment[2] = str(result.humidity)[3]
-            segment.set_digit_raw(3, 0b01110011)
-        segment.show()
+            # Zeigt die gemessenen Werte im Terminal an.
+            addDataLineToTerminal(0, "Temperatur:", f"{result.temperature} C")
+            addDataLineToTerminal(1, "Feuchtigkeit:", f"{result.humidity} %")
+            addDataLineToTerminal(2, "Messung:", f"{measurements}")
+            stdscr.refresh()
 
-        lcd.message = f"Temperatur:{result.temperature}C\nFeuchte:   {result.humidity}%"
+            # Aktualisiert das 7-Segment-Display mit den Sensorwerten.
+            if change == 0:
+                segment[0] = str(result.temperature)[0]
+                segment[1] = str(result.temperature)[1]
+                segment[1] = str(result.temperature)[2]
+                segment[2] = str(result.temperature)[3]
+                segment[3] = 'C'
+            else:
+                segment[0] = str(result.humidity)[0]
+                segment[1] = str(result.humidity)[1]
+                segment[1] = str(result.humidity)[2]
+                segment[2] = str(result.humidity)[3]
+                segment.set_digit_raw(3, 0b01110011)
+            segment.show()
 
+            lcd.message = f"Temperatur:{result.temperature}C\nFeuchte:   {result.humidity}%"
+
+    thread_light_sensor = multiprocessing.Process(target=startLightSensor)
+    thread_temp_humid_sensor = multiprocessing.Process(target=startTempAndHumidSensor)
+
+    thread_light_sensor.start()
+    thread_temp_humid_sensor.start()
 
 # Der Hauptteil des Codes. Hier wird die curses-Bibliothek verwendet, um das Terminal-UI zu erstellen.
 if __name__ == '__main__':
     try:
         curses.wrapper(main)
     except KeyboardInterrupt:
+        programm_run = False
         segment.fill(0)
         lcd.clear()
         lcd.backlight = False
+        thread_light_sensor.join()
+        thread_temp_humid_sensor.join()
